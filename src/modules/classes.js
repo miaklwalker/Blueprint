5701c1c61a3e92f29f2add0044cade6dcd4e251c
Michael Walker <ouisihai@gmail.com>
2025-02-22 13:17:05 -0600

    feat: Shopping list p1 complete

---
+++ src/modules/classes.js
@@ -0,0 +1,8 @@
+export class Buy{
+    constructor({ ante, blind, location, cardName }){
+        this.ante=ante;
+        this.blind=blind;
+        this.location=location;
+        this.cardName=cardName;
+    }
+}
